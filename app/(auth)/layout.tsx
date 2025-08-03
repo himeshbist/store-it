2048b6da92eee2db8998cc9679b992f7b97978c0
Himesh Bist <himesh_2312res299@iitp.ac.in>
2025-08-03 20:29:17 +0000

    3 push

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,11 +13,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             height={82}
             className="h-auto"
           />
+
           <div className="space-y-5 text-white">
-            <h1 className="h1"> Manage your files the best way</h1>
+            <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
-              {" "}
-              This is a place where you store all your documents
+              This is a place where you can store all your documents.
             </p>
           </div>
           <Image
@@ -29,16 +29,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           />
         </div>
       </section>
-      <section className="flex flex-1 items-center bg-white p-4 py-10 lg:justify-center lg:p-10">
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
             src="/assets/icons/logo-full-brand.svg"
             alt="logo"
             width={224}
             height={82}
-            className="h-auto w-[200px] lg:w-[250]"
+            className="h-auto w-[200px] lg:w-[250px]"
           />
         </div>
+
         {children}
       </section>
     </div>
